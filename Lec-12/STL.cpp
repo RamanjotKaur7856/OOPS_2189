5ff10c2e90e8a4d53dd41699b0be6a7f1fc6a42c
RamanjotKaur7856 <156178826+RamanjotKaur7856@users.noreply.github.com>
2024-09-03 15:07:21 +0530

    Vector

--- Lec-12/STL.cpp
+++ Lec-12/STL.cpp
@@ -4,36 +4,23 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-//int arr[5];
-//int arr[5]={1};
-//int main(){
-//	int arr[5];//Garbage value
-//	for(int i=0;i<5;i++){
-//		cout<<arr[i];
-//	}
-//	int arr[5]={1};
-//    int arr[5];
-//    array<int,5>arr={1};
-//	for(int i=0;i<5;i++){
-//		cout<<arr[i];
-//	}
-//	for(auto it=arr.begin();it!=arr.end();it++){
-//		cout<<*it<<endl;
-//	}
-//	return 0;
-//}
-
-
-//begin,end,rbegin,rend,at,fill,front,back,size
+int arr[5];
+int arr[5]={1};
 int main(){
- vector<int> v;
-for(int i=0;i<5;i++){
-		int element;
-		cin>>element;
-		v.push_back(element);
+	int arr[5];//Garbage value
+	for(int i=0;i<5;i++){
+		cout<<arr[i];
 	}
-    // using begin() to print vector
-    for (auto it = v.begin();it != v.end(); ++it)
-        cout << ' ' << *it;
-    return 0;
+	int arr[5]={1};
+    int arr[5];
+    array<int,5>arr={1};
+	for(int i=0;i<5;i++){
+		cout<<arr[i];
+	}
+	for(auto it=arr.begin();it!=arr.end();it++){
+		cout<<*it<<endl;
+	}
+	return 0;
 }
+
+
